d127fcb991e36937c149615e15f030aa88ffe847
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-08-29 13:53:10 +0600

    water background

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@ $(window).scroll(function(){
         $('.menu').removeClass('fixed-top');
     }
 });
+
+
+var wish_active = document.getElementById("wish-active");
+var wish_inactive = document.getElementById("wish-inactive");
+
+$(".wish-active").click(function(){
+  wish_active.style.display = "none";
+  wish_inactive.style.display = "block";
+  
+});
+
+$(".wish-inactive").click(function(){
+  wish_inactive.style.display = "none";
+  wish_active.style.display = "block";
+  
+});
+
+
+
