13b29d9ad8303ffd39373f0263de0713bd1dcc3f
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-09-07 13:51:45 +0600

    menu issue fixed and responsive

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-
-$(window).scroll(function(){
+$(window).scroll(function() {
     if ($(window).scrollTop() >= 50) {
         $('.menu').addClass('fixed-top');
-    }
-    else {
+    } else {
         $('.menu').removeClass('fixed-top');
     }
 });
 
 var newcolor = document.getElementById('wish-active')
-$('.wish-active').click(function(){
- 
+$('.wish-active').click(function() {
+
     $(this).find('i').toggleClass('fas far');
-    
+
 
 });
 
@@ -20,165 +18,41 @@ $('body').scrollspy({ target: '#get-now' });
 
 
 
-(function ($) {
-$(document).ready(function() {
-$('.xzoom, .xzoom-gallery').xzoom({zoomWidth: 400, title: true, tint: '#333', Xoffset: 15});
-$('.xzoom2, .xzoom-gallery2').xzoom({position: '#xzoom2-id', tint: '#ffa200'});
-$('.xzoom3, .xzoom-gallery3').xzoom({position: 'lens', lensShape: 'circle', sourceClass: 'xzoom-hidden'});
-$('.xzoom4, .xzoom-gallery4').xzoom({tint: '#006699', Xoffset: 15});
-$('.xzoom5, .xzoom-gallery5').xzoom({tint: '#006699', Xoffset: 15});
 
-//Integration with hammer.js
-var isTouchSupported = 'ontouchstart' in window;
-
-if (isTouchSupported) {
-//If touch device
-$('.xzoom, .xzoom2, .xzoom3, .xzoom4, .xzoom5').each(function(){
-var xzoom = $(this).data('xzoom');
-xzoom.eventunbind();
-});
-
-$('.xzoom, .xzoom2, .xzoom3').each(function() {
-var xzoom = $(this).data('xzoom');
-$(this).hammer().on("tap", function(event) {
-event.pageX = event.gesture.center.pageX;
-event.pageY = event.gesture.center.pageY;
-var s = 1, ls;
-
-xzoom.eventmove = function(element) {
-element.hammer().on('drag', function(event) {
-event.pageX = event.gesture.center.pageX;
-event.pageY = event.gesture.center.pageY;
-xzoom.movezoom(event);
-event.gesture.preventDefault();
-});
-}
-
-xzoom.eventleave = function(element) {
-element.hammer().on('tap', function(event) {
-xzoom.closezoom();
-});
-}
-xzoom.openzoom(event);
-});
-});
-
-$('.xzoom4').each(function() {
-var xzoom = $(this).data('xzoom');
-$(this).hammer().on("tap", function(event) {
-event.pageX = event.gesture.center.pageX;
-event.pageY = event.gesture.center.pageY;
-var s = 1, ls;
-
-xzoom.eventmove = function(element) {
-element.hammer().on('drag', function(event) {
-event.pageX = event.gesture.center.pageX;
-event.pageY = event.gesture.center.pageY;
-xzoom.movezoom(event);
-event.gesture.preventDefault();
-});
-}
-
-var counter = 0;
-xzoom.eventclick = function(element) {
-element.hammer().on('tap', function() {
-counter++;
-if (counter == 1) setTimeout(openfancy,300);
-event.gesture.preventDefault();
-});
-}
-
-function openfancy() {
-if (counter == 2) {
-xzoom.closezoom();
-$.fancybox.open(xzoom.gallery().cgallery);
-} else {
-xzoom.closezoom();
-}
-counter = 0;
-}
-xzoom.openzoom(event);
-});
-});
-
-$('.xzoom5').each(function() {
-var xzoom = $(this).data('xzoom');
-$(this).hammer().on("tap", function(event) {
-event.pageX = event.gesture.center.pageX;
-event.pageY = event.gesture.center.pageY;
-var s = 1, ls;
-
-xzoom.eventmove = function(element) {
-element.hammer().on('drag', function(event) {
-event.pageX = event.gesture.center.pageX;
-event.pageY = event.gesture.center.pageY;
-xzoom.movezoom(event);
-event.gesture.preventDefault();
-});
-}
-
-var counter = 0;
-xzoom.eventclick = function(element) {
-element.hammer().on('tap', function() {
-counter++;
-if (counter == 1) setTimeout(openmagnific,300);
-event.gesture.preventDefault();
-});
-}
-
-function openmagnific() {
-if (counter == 2) {
-xzoom.closezoom();
-var gallery = xzoom.gallery().cgallery;
-var i, images = new Array();
-for (i in gallery) {
-images[i] = {src: gallery[i]};
-}
-$.magnificPopup.open({items: images, type:'image', gallery: {enabled: true}});
-} else {
-xzoom.closezoom();
-}
-counter = 0;
-}
-xzoom.openzoom(event);
-});
-});
-
-} else {
-//If not touch device
-
-//Integration with fancybox plugin
-$('#xzoom-fancy').bind('click', function(event) {
-var xzoom = $(this).data('xzoom');
-xzoom.closezoom();
-$.fancybox.open(xzoom.gallery().cgallery, {padding: 0, helpers: {overlay: {locked: false}}});
-event.preventDefault();
-});
-
-//Integration with magnific popup plugin
-$('#xzoom-magnific').bind('click', function(event) {
-var xzoom = $(this).data('xzoom');
-xzoom.closezoom();
-var gallery = xzoom.gallery().cgallery;
-var i, images = new Array();
-for (i in gallery) {
-images[i] = {src: gallery[i]};
-}
-$.magnificPopup.open({items: images, type:'image', gallery: {enabled: true}});
-event.preventDefault();
-});
-}
-});
-})(jQuery);
 
 
 
 $(document).ready(function() {
 
-$('.detail-color-box').on('click', 'img', function() {
-    $('.detail-color-box img.color-active').removeClass('color-active');
-    $(this).addClass('color-active');
-});
+    $('.detail-color-box').on('click', 'img', function() {
+        $('.detail-color-box img.color-active').removeClass('color-active');
+        $(this).addClass('color-active');
+    });
 });
 
 
+
+
+
+
+
+
+$(document).ready(function() {
+    if (window.innerWidth < 576) {
+        $('.card-flex-all').addClass('card-flex-xs').removeClass('card-flex-md');
+    } else if (window.innerWidth < 768) {
+        $('.card-flex-all').addClass('card-flex-sm').removeClass('card-flex-md');
+    } else {
+        $('.card-flex-all').addClass('card-flex-md');
+    };
+});
+
+$(document).ready(function() {
+    if (window.innerWidth < 576) {
+        $('.card-flex-all-8').addClass('card-flex-xs-8').removeClass('card-flex-md-8');
+    } else if (window.innerWidth < 768) {
+        $('.card-flex-all-8').addClass('card-flex-sm-8').removeClass('card-flex-md-8');
+    } else {
+        $('.card-flex-all-8').addClass('card-flex-md-8');
+    };
+});
