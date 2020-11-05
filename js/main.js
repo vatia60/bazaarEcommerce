ea18b66654e6ba37b1437837a862d1a537aa1e8c
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-11-05 18:43:32 +0600

    desktop again n12

--- js/main.js
+++ js/main.js
@@ -258,6 +258,16 @@ Product details page product image zoom detailproduct.html
             self.append(layer).append(showPanel);
 
             self.on('mousemove', function(e) {
+                defaultParas.showPanelW = imageW;
+                selfOffset = self.offset();
+                console.log(self.offset())
+                imageW = self.width();
+                img.css({
+                    width: imageW * wTimes
+                })
+                showPanel.css({
+                    width: imageW
+                })
 
                 var x = e.pageX - selfOffset.left;
                 var y = e.pageY - selfOffset.top;
@@ -299,8 +309,9 @@ Product details page product image zoom detailproduct.html
 })(jQuery);
 
 
+if (window.innerWidth < 992) {
     $('.showhope').zoomImage();
-
+}
 
 $('.show-small-img:first-of-type').css({
     'border': 'solid 1px gray',
